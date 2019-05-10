b15ff39881dbcddbb9a00077c416a2350113cd69
cnguyen0691 <chaunguyen0691@gmail.com>
2019-05-10 12:57:31 -0400

    second

--- src/main/java/com/example/lesson01/HomeController.java
+++ src/main/java/com/example/lesson01/HomeController.java
@@ -6,13 +6,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class HomeController {
-    @RequestMapping("/")
-    public String homepage(){
-        return "index";
-    }
     @RequestMapping("/Crabby")
     public String Crabby (){
         return "Crabby";
+    }
+    @RequestMapping("/index")
+    public String homepage(){
+        return "index";
+    }
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+    @RequestMapping("/event")
+    public String event(){
+        return "event";
+    }
+    @RequestMapping("/traveling")
+    public String traveling(){
+        return "traveling";
+    }
+    @RequestMapping("/social")
+    public String social(){
+        return "social";
     }
 
 }
